67f98839db2003617f10ad73ddbe981a2879a4dd
Christian Jones <chjones@aleph0.com>
2021-01-31 17:02:29 -0500

    Initial incomplete handling of attachments
    Doesn't actually rewrite and resend all
    messages (e.g., inline attachments
    aren't managed) but establishes framework
    for doing so.
    Additionally cleans up better, and moves
    unsuccessful messages to a mailbox on
    the source server.

--- imap2gmail.py
+++ imap2gmail.py
@@ -1,5 +1,5 @@
 # imap2gmail
-# version 20210129-1
+# version 20210131-1
 #
 # Move messages via IMAP from 
 # imaps://sourceAccount:sourcePassword@sourceServer:sourcePort/sourceMailbox
@@ -14,19 +14,25 @@
 #
 # See also NOTES
 
-import imaplib, email
+import imaplib, os
+import email
+import email.policy, email.message
 
 sourceAccount = '[the source account username is not set]'
 sourcePassword = ''
 sourceServer = '[the source account server address is not set]'
 sourcePort = 993
 sourceMailbox = 'INBOX'
+sourceErrorMailbox = '[the source error mailbox is not set]'
 destAccount = '[the destination account username is not set]'
 destPassword = ''
 destServer = '[the destination account server address is not set]'
 destPort = 993
 destMailbox = 'INBOX'
 destMaxSize = 25000000
+attachmentDir = '[the local attachment directory is not set]'
+
+msgPolicy = email.policy.SMTP.clone(refold_source='none')
 
 source = imaplib.IMAP4_SSL(sourceServer, sourcePort)
 source.login( sourceAccount, sourcePassword )
@@ -34,35 +40,64 @@ result, [ numMessages ] = source.select( sourceMailbox )
 if numMessages != b'0' :
     dest = imaplib.IMAP4_SSL(destServer, destPort)
     dest.login( destAccount, destPassword )
-    result, discard = dest.select( destMailbox )
-
-    newMessages = list()
+    result = dest.select( destMailbox )
+    
     result, [ nums ] = source.uid('search',None,'ALL')
     msgUids = nums.split()
     
     for msgUid in msgUids:
+        # Instead of checking all this first, should we just attempt to put it
+        # on the destination and handle problems if not appendable?
         # Two calls may be expensive, but since we have to futz with msgDate
         # it minimizes memory use in the case that data is very large
         result, data = source.uid('fetch', msgUid, 'RFC822')
         result, [ msgDate ] = source.uid('fetch', msgUid, 'INTERNALDATE')
 
-        newData = data[0][1] # the actual message data
-
         # to keep imaplib from trying to interpret msgDate when appending
         # to the new mailbox, it must be in double quotes
         msgDate = '"' + msgDate.split(b'"')[1].decode() + '"'
+
+        newData = data[0][1] # the actual message data
         if len(newData) > destMaxSize:
-            newMsg = email.message_from_bytes( newData )
-            # if message size too large, check if there are attachments
+            newMsg = email.message_from_bytes( newData, None, policy=msgPolicy )
             if newMsg.is_multipart():
-                # if there are attachments, check their sizes
-                # see if some combination of removal of attachments results in a message
-                #  that's not too big 
-                # then rewrite message, post attachments elsewhere, etc (probably via
-                #  a separate function so I can then also use it if adding to dest
-                #  fails)
+                # some sort of id, could be based on boundary or some content-id
+                msgBoundary = newMsg.get_boundary()
+                for msgPart in newMsg.walk() :
+                    if msgPart.get_content_disposition() != None :
+                        # get size could include using the size param of 
+                        # the content-disposition or something else
+                        msgHeaders = msgPart.items()
+                        attName = msgPart.get_filename('attachment') # probably need to make these things 'safe'
+                        attDate = msgPart['Content-Disposition'].params['modification-date']
+                        if attDate == None:
+                            attDate = msgPart['Content-Dispositon'].params['creation-date']
+                        attPath = os.path.join(attachmentDir, attName)
+                        with open(attPath,'wb') as attFile:
+                            attFile.write( msgPart.get_content() )
+                        # modify attachment to have appropriate date
+                        msgString = 'This email has been modfied from the original, which '
+                        msgString += 'was too large to deliver. The following attachment '
+                        msgString += "was removed:\n\n"
+                        msgString += "Original headers:\n"
+                        for headerName, headerValue in msgHeaders:
+                            msgString += headerName + ' : ' + headerValue + "\n"
+                        msgString += "\nAttachent saved as:\n"
+                        msgString += attPath + "\n"
+                        msgPart.set_content(msgString, disposition='inline')
+            msgFrom = bool( newMsg.get_unixfrom() )
+            newData = newMsg.as_bytes(msgFrom)
+            if len(newData) > destMaxSize:
                 True
         result, data = dest.append(destMailbox,None,msgDate,newData)
         if result == b'OK':
             source.uid('store', msgUid, '+FLAGS', '\\Deleted')
-    result, discard = source.expunge()
+        else:
+            if sourceMailbox != sourceErrorMailbox:
+                result = source.uid('copy', msgUid, sourceErrorMailbox)
+                if result[0] == b'OK':
+                    source.uid('store', msgUid, '+FLAGS', '\\Deleted')
+    dest.close()
+    dest.logout()
+source.close()
+source.logout()
