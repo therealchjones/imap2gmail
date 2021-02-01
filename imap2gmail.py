799047da60bf01b0b5a1f61dbaad06937faf99ec
Christian Jones <chjones@aleph0.com>
2021-02-01 17:42:54 -0500

    Make basically usable
    Attempt to append messages, then
    print error if it fails. This now supersedes
    the capabilities I'm using in movemail
    and I'll use this.

--- imap2gmail.py
+++ imap2gmail.py
@@ -1,11 +1,12 @@
 # imap2gmail
-# version 20210131-1
+# version 20210201-1
 #
 # Move messages via IMAP from 
 # imaps://sourceAccount:sourcePassword@sourceServer:sourcePort/sourceMailbox
 # to 
 # imaps://destAccount:destPassword@destServer:destPort/destMailbox
-# The parameters for these IMAP-TLS URLs can be set just below.
+# The parameters for these IMAP-TLS URLs can be set just below in the
+# USER ACCOUNT CONFIGURATION section
 # TODO: 
 # - handle messages that are too large or otherwise unable to be 
 #   appended to the destination mailbox
@@ -14,13 +15,10 @@
 #
 # See also NOTES
 
-import imaplib, os
-import email
-import email.policy, email.message
-
+# USER ACCOUNT CONFIGURATION ------------------------------------------
 sourceAccount = '[the source account username is not set]'
-sourcePassword = ''
-sourceServer = '[the source account server address is not set]'
+sourcePassword = '[the source account password is not set]'
+sourceServer = '[the source account server is not set]'
 sourcePort = 993
 sourceMailbox = 'INBOX'
 sourceErrorMailbox = '[the source error mailbox is not set]'
@@ -29,19 +27,28 @@ destPassword = ''
 destServer = '[the destination account server address is not set]'
 destPort = 993
 destMailbox = 'INBOX'
-destMaxSize = 25000000
+destMaxSize = 25000000 # bytes
+attachmentRewrite = False # True, False, or None
 attachmentDir = '[the local attachment directory is not set]'
+errorBehavior = 'move' # 'move' to sourceErrorMailbox, 
+                       # 'save' in sourceMailbox (will be seen next time)
+# end of USER ACCOUNT CONFIGURATION -----------------------------------
 
-msgPolicy = email.policy.SMTP.clone(refold_source='none')
+# nothing below here is end-user configurable -------------------------
+import imaplib, os, sys
+import email
+import email.policy, email.message
 
 source = imaplib.IMAP4_SSL(sourceServer, sourcePort)
-source.login( sourceAccount, sourcePassword )
+result = source.login( sourceAccount, sourcePassword )
 result, [ numMessages ] = source.select( sourceMailbox )
 if numMessages != b'0' :
+    msgPolicy = email.policy.SMTP.clone(refold_source='none')
+
     dest = imaplib.IMAP4_SSL(destServer, destPort)
     dest.login( destAccount, destPassword )
     result = dest.select( destMailbox )
-    
+
     result, [ nums ] = source.uid('search',None,'ALL')
     msgUids = nums.split()
     
@@ -58,7 +65,7 @@ if numMessages != b'0' :
         msgDate = '"' + msgDate.split(b'"')[1].decode() + '"'
 
         newData = data[0][1] # the actual message data
-        if len(newData) > destMaxSize:
+        if ( len(newData) > destMaxSize ) & bool(attachmentRewrite):
             newMsg = email.message_from_bytes( newData, None, policy=msgPolicy )
             if newMsg.is_multipart():
                 # some sort of id, could be based on boundary or some content-id
@@ -89,14 +96,34 @@ if numMessages != b'0' :
             newData = newMsg.as_bytes(msgFrom)
             if len(newData) > destMaxSize:
                 True
-        result, data = dest.append(destMailbox,None,msgDate,newData)
-        if result == b'OK':
-            source.uid('store', msgUid, '+FLAGS', '\\Deleted')
-        else:
-            if sourceMailbox != sourceErrorMailbox:
+        try:
+            # for Google's standard 'BAD' (TOOBIG) response for a message
+            # that's too large, an exception is thrown rather than returning
+            # the result
+            result, data = dest.append(destMailbox,None,msgDate,newData)
+            if result == 'OK': 
+                source.uid('store', msgUid, '+FLAGS', '\\Deleted')
+            else:
+                raise imaplib.IMAP4.error
+        except imaplib.IMAP4.error as exception:
+            # Unfortunately this doesn't work since an exception is raised
+            # and it's not reliable to parse the result, but maybe for gmail
+            # specifically we can use the 'TOOBIG' tag? The exception seems
+            # to have only the string with no other data
+            print('Error moving message with uid ' + msgUid.decode() + '(message date ' + msgDate + ')' ,file=sys.stderr)
+            print(' from ' + sourceAccount + '@' + sourceServer + '/' + sourceMailbox,file=sys.stderr)
+            print(' to ' + destAccount + '@' + destServer + '/' + destMailbox,file=sys.stderr)
+            print(' (' + str(exception) + ')' )
+
+            if ( sourceMailbox != sourceErrorMailbox ) & ( errorBehavior == 'move' ):
                 result = source.uid('copy', msgUid, sourceErrorMailbox)
                 if result[0] == b'OK':
+                    print(' Moved it to ' + sourceAccount + '@' + sourceServer + '/' + sourceErrorMailbox + ' instead.', file=sys.stderr)
                     source.uid('store', msgUid, '+FLAGS', '\\Deleted')
+                else:
+                    print(' Left it in place instead.', file=sys.stderr)
+            else:
+                print(' Left it in place instead.', file=sys.stderr)
     dest.close()
     dest.logout()
 source.close()
